6221de2a355b08915aa01e3f8cc691b375256cd8
Ifedayo Awoniyi <iawoniyi@lagetronix.com>
2023-11-10 12:47:50 +0100

    Add project files.

---
+++ VersityHub/VersityHubWebAPI/Customer/Model/CustomerModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace VersityHub.VersityHubWebAPI.Customer.CustomerModel
+{
+    public class CustomerModel
+    {
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+      
+    }
+}
---
+++ VersityHub/VersityHubWebAPI/Customer/Seller/CreateSellerAccount.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VersityHub.VersityHubWebAPI.Customer.Seller
+{
+    public class CreateSellerAccount
+    {
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Phone Number is required")]
+        [RegularExpression(@"^\+234\d{10}$", ErrorMessage = "Invalid Phone Number format. It should start with +234 and have 10 additional digits.")]
+        public string PhoneNumber { get; set; }
+
+        public string Gender { get; set; }
+
+        public string State { get; set; }
+
+        public string University { get; set; }
+
+        public string UniversityID { get; set; }
+
+        [Required(ErrorMessage = "Date of Birth is required")]
+        [RegularExpression(@"^\d{4}-\d{2}-\d{2}$", ErrorMessage = "Invalid Date of Birth format")]
+        public string DateOfBirth { get; set; }
+    }
+}
